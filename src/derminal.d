ad443c66340d583a030fa892d89b9785634efb19
deadalnix <deadalnix@gmail.com>
2013-09-19 22:09:50 +0900

    Run terminal asynchronously

--- src/derminal.d
+++ src/derminal.d
@@ -1,16 +1,28 @@
 module derminal;
 
+import glib.Idle;
+
 import vte.Terminal;
 
 final class Derminal : Terminal {
 	GPid pid;
+	Idle idle;
 	
 	this(string folder) {
 		super();
 		setScrollbackLines(2048);
 		setMouseAutohide(true);
 		
-		forkCommandFull(VtePtyFlags.DEFAULT, folder, [Terminal.getUserShell()], [], cast(GSpawnFlags) 0, null, null, pid);
+		// Idle in order to get all callback ready before launching the terminal.
+		idle = new Idle({
+			// So it can be garbage collected.
+			idle = null;
+			
+			// Start terminal.
+			forkCommandFull(VtePtyFlags.DEFAULT, folder, [Terminal.getUserShell()], [], cast(GSpawnFlags) 0, null, null, pid);
+			
+			return false;
+		});
 	}
 	
 	string getCurrentFolder() {
